3d5b49f9f544fdc1e4b6bea63ea2dc1d350c7403
pirum <85345134+IGr-Code@users.noreply.github.com>
2024-06-27 14:17:33 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,6 +1,21 @@
+import os
+import string
+import exiftool
+
 
 
 def caesar():
+    
+    #Aditional functions
+    def is_symbol(char):
+        # Define a string containing all symbols
+        symbols = string.punctuation  # Includes !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+        # Check if the character is in the symbols string
+        return char in symbols
+    
+    
+    
     print('''
           ⠀⠀⠀⠀⠀⠀⢀⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -81,9 +96,9 @@ def caesar():
                 if char.isalpha():  # Check if character is alphabetic
                     
                     if char.islower():  # Check if character is lowercase
-                        new_char = chr((ord(char) - ord('a') + shift) % eng + ord('a'))
+                        new_char = chr((ord(char) - ord('a') - shift) % eng + ord('a'))
                     else:  # Character is uppercase
-                        new_char = chr((ord(char) - ord('A') + shift) % eng + ord('A'))
+                        new_char = chr((ord(char) - ord('A') - shift) % eng + ord('A'))
         
                 else:  # Character is a symbol or whitespace or digit
                     new_char = char
@@ -96,9 +111,13 @@ def caesar():
         # Start BruteForse mode        
         elif brute_forse == True:
             
+            result = ''
+            
             for shift in range(eng):
                 
                 encrypted_text = ""  # Initialize encrypted text for each shift
+                
+                
                 
                 
                 for char in text:
@@ -106,17 +125,50 @@ def caesar():
                     if char.isalpha():  # Check if character is alphabetic
                     
                         if char.islower():  # Check if character is lowercase
-                            new_char = chr((ord(char) - ord('a') + shift) % eng + ord('a'))
+                            new_char = chr((ord(char) - ord('a') - shift) % eng + ord('a'))
                         else:  # Character is uppercase
-                           new_char = chr((ord(char) - ord('A') + shift) % eng + ord('A'))
+                           new_char = chr((ord(char) - ord('A') - shift) % eng + ord('A'))
         
                     else:  # Character is a symbol or whitespace or digit
                         new_char = char
 
                     
                     encrypted_text += new_char
+
+                result = result + f'\n[Shift {eng - shift}] {encrypted_text}'
+                print(f'\n[Shift {shift}] {encrypted_text}')
+                
+                
+            # Save result option prompt
+            print('\n ---------SAVE?----------- ')
+            print("\nTo save results, type a file path (e.g., example_name.txt) or type 'n' to not save:")
+
+            # Get file path from user input
+            file_path = input('>>> ')
+
+            # Check if the user opted not to save
+            if file_path.lower() == 'n':
+                print("Results not saved.")
+            else:
+                try:
+                    # Attempt to save or append to the specified file
+                    if not os.path.exists(file_path):
+                        # If the file doesn't exist, create it
+                        with open(file_path, 'w') as file:
+                            file.write(result)
+                            print(f"File '{file_path}' created and text saved.")
+                    else:
+                        # If the file exists, append to it
+                        with open(file_path, 'a') as file:
+                            file.write("\n" + result)  # Append text to existing file
+                            print(f"Text appended to '{file_path}'.")
+
+                except Exception as e:
+                    # Handle any exceptions that occur during file operation
+                    print(f"Error saving results to '{file_path}': {str(e)}")
             
-                print(f'\n[Shift {eng - shift}] {encrypted_text}')
+            
+            
             
              
         #Close programme
@@ -131,6 +183,13 @@ def caesar():
     
     
     
+from exiftool import ExifToolHelper
+   
+def meta_data():
+    with ExifToolHelper() as et:
+        for d in et.get_metadata("DSCN0010.jpg"):
+            for k, v in d.items():
+                print(f"Dict: {k} = {v}") 
     
 
 
--- pirum.py
+++ pirum.py
@@ -2,19 +2,22 @@
 # pirum.py
 
 import argparse
-from functions import caesar
+from functions import caesar, meta_data
 
 
 
 def main():
     parser = argparse.ArgumentParser(description='A simple program that does something.')
     parser.add_argument('-ceasar', action='store_true', help='Show tutorial on how to use this program')
-    
+    parser.add_argument('-metadata', action='store_true', help='Show tutorial on how to use this program')
+  
     
     args = parser.parse_args()
     
     if args.ceasar:
         caesar()
+    elif args.metadata:
+        meta_data()
     else:
         print("Hello, Pirum!")
 
